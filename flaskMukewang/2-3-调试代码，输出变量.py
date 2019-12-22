4708c05a81ac34341fb1105b93eb3ee17498d151
tongyue2018 <tongyue2018@163.com>
2019-12-22 17:56:33 +0800

    nohup

--- flaskMukewang/2-3-调试代码，输出变量.py
+++ flaskMukewang/2-3-调试代码，输出变量.py
@@ -1,17 +1,17 @@
 from flask import Flask
-import sys
+import logging
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
 
 @app.route('/')
 def register():
+    app.logger.info('123')
     return 'hello world'
 
 if(__name__ == '__main__'):
-    app.logger.debug('1232')
-    app.logger.warning('123')
-    app.logger.error('123')
-    app.logger.info('123')
+    handler = logging.FileHandler('flask.log')
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+    app.run(host=app.config['HOST'],port=5000,debug=True)
 
-    app.run(host=app.config['HOST'],port=5000,debug=1)
