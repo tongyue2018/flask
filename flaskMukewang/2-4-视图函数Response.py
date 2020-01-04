af08db1478154f5e07879b6e10db30bbcb0b2898
tongyue2018 <tongyue2018@163.com>
2020-01-04 22:27:35 +0800

    nohup

--- flaskMukewang/2-4-视图函数Response.py
+++ flaskMukewang/2-4-视图函数Response.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,make_response
 import logging
 
 app = Flask(__name__)
@@ -7,7 +7,22 @@ app.config.from_pyfile('../config.py')
 @app.route('/')
 def register():
     app.logger.info('hello')
-    return 'hello world'
+    # response = make_response('<h1>hello</h1>') #默认返回html
+    # response.headers['Content-type'] = 'text/plain' #返回普通字符
+
+    response = make_response('<h1>hello world</h1>')
+    # response = make_response('<h1>hello world</h1>',301) #返回码301时重定向到location的地址
+
+    response.statusCode = 200
+    headers = {
+        'location':'https://www.baidu.com',
+        'Content-Type':'text/html'
+        # 'Content-Type':'text/plain'
+
+    }
+    response.headers = headers
+    app.logger.info(response)
+    return response
 
 if(__name__ == '__main__'):
     logging.basicConfig(
@@ -18,3 +33,12 @@ if(__name__ == '__main__'):
         format='%(asctime)s - %(levelname)s: %(message)s'  # 日志格式
     )
     app.run(host=app.config['HOST'],port=5000,debug=True)
+#
+# 一、视图函数默认status code:200
+# 可返回301和 location:"重定向地址"
+#
+# 二、视图函数默认header，第一个html
+# Content-Type:text/html
+# Content-Type:application/json
+# Content-Type:text/plain --普通字符
+# Content-Type:application/x-www-form-urlencoded -- 一般requests请求form表单接口时候用到。
