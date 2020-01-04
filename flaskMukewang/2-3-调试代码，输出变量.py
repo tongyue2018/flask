60ec9f1c2e8485e55b0b43fdcb2c2e37fef8c333
tongyue2018 <tongyue2018@163.com>
2020-01-04 21:31:31 +0800

    nohup

--- flaskMukewang/2-3-调试代码，输出变量.py
+++ flaskMukewang/2-3-调试代码，输出变量.py
@@ -1,16 +1,8 @@
 from flask import Flask
 import logging
-logging.basicConfig(
-    level=logging.DEBUG,#控制台打印的日志级别
-    filename='flask.log',  # 将日志写入log_new.log文件中
-    filemode='a',##模式，a追加，w覆盖
-    # format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'#完整日志格式
-    format='%(asctime)s - %(levelname)s: %(message)s'#日志格式
-)
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
-
 
 @app.route('/')
 def register():
@@ -18,6 +10,13 @@ def register():
     return 'hello world'
 
 if(__name__ == '__main__'):
+    logging.basicConfig(
+        level=logging.DEBUG,  # 控制台打印的日志级别
+        filename='/Users/tingtingli/Documents/work-tongyue/flask/flaskMukewang/flask.log',  # 将日志写入log_new.log文件中
+        filemode='a',  ##模式，a追加，w覆盖
+        # format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'#完整日志格式
+        format='%(asctime)s - %(levelname)s: %(message)s'  # 日志格式
+    )
     app.run(host=app.config['HOST'],port=5000,debug=True)
 
 # 第一步：加入logging.basicConfig基础配置。
--- flaskMukewang/2-4-视图函数Response.py
+++ flaskMukewang/2-4-视图函数Response.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
-
+app.config.from_pyfile('../config.py')
 
 @app.route('/')
 def register():
-    return 'hello'
+    app.logger.info('hello')
+    return 'hello world'
 
 if(__name__ == '__main__'):
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    logging.basicConfig(
+        level=logging.DEBUG,  # 控制台打印的日志级别
+        filename='flask.log',  # 将日志写入log_new.log文件中
+        filemode='w',  ##模式，a追加，w覆盖
+        # format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'#完整日志格式
+        format='%(asctime)s - %(levelname)s: %(message)s'  # 日志格式
+    )
+    app.run(host=app.config['HOST'],port=5000,debug=True)
