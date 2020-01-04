116fd7e5a53560e2cc4d26c070213fd528c6abd4
tongyue2018 <tongyue2018@163.com>
2020-01-04 21:32:07 +0800

    nohup

--- flaskMukewang/2-3-调试代码，输出变量.py
+++ flaskMukewang/2-3-调试代码，输出变量.py
@@ -12,7 +12,7 @@ def register():
 if(__name__ == '__main__'):
     logging.basicConfig(
         level=logging.DEBUG,  # 控制台打印的日志级别
-        filename='/Users/tingtingli/Documents/work-tongyue/flask/flaskMukewang/flask.log',  # 将日志写入log_new.log文件中
+        filename='flask.log',  # 将日志写入log_new.log文件中
         filemode='a',  ##模式，a追加，w覆盖
         # format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'#完整日志格式
         format='%(asctime)s - %(levelname)s: %(message)s'  # 日志格式
