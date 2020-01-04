1083ae060d2496420d77143d2c96a02ef147ce0c
tongyue2018 <tongyue2018@163.com>
2020-01-04 20:40:39 +0800

    nohup

---
+++ flaskMukewang/2-5-__name__作用.py
@@ -0,0 +1,9 @@
+
+
+# if(__name__ == '__main__'):
+#     app.run(host='0.0.0.0',port=5000,debug=True)
+
+
+# 1.其他模块导入，不被其他模块调用。
+# 2.nginx+uwsgi服务器搭建时，不会使用flask内部服务器app.run，而是使用外部服务器uwsgi启动flask相关代码。
+
