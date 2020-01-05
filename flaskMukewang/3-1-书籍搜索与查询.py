080478672f1c0810bc6851234a1e59e18c9c79b3
tongyue2018 <tongyue2018@163.com>
2020-01-05 20:20:33 +0800

    nohup

---
+++ flaskMukewang/3-1-书籍搜索与查询.py
@@ -0,0 +1,23 @@
+
+#根据isbn搜索
+#根据书籍名称搜索
+
+from flask import Flask,make_response
+
+app = Flask(__name__)
+app.config.from_pyfile('../config.py')
+
+
+@app.route('/')
+def register():
+    response = make_response('<h1>hello world</h1>')
+    headers = {
+        'content-type':'text/plain' #说明不区分大小写
+    }
+    response.headers = headers
+    return response
+
+
+if(__name__ == '__main__'):
+    app.run(app.config['HOST'],5000,debug=True)
+
