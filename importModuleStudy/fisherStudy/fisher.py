fc609186191777a439dfd873827678f6f6a770d7
tongyue2018 <tongyue2018@163.com>
2020-02-03 19:41:51 +0800

    nohup

--- importModuleStudy/fisherStudy/fisher.py
+++ importModuleStudy/fisherStudy/fisher.py
@@ -16,6 +16,9 @@ print(sys.path)
 # 导包的两种方式
 # 一、在同一文件夹下，直接导入文件名字即可，命令行 和 pycharm执行都没问题
 # 二、如果不在同一文件夹下，需要从项目根目录开始写（不需要写项目目录名称） 如 importModuleStudyDic.fisherStudy.helper
+# 方式二importModuleStudyDic.fisherStudy.helper命令行执行会报错 找不到包
+# 三、命令行执行，sys.path只有当前文件执行目录 没有项目目录，因此可能需要main.py方式解决。
+
 from importModuleStudy.fisherStudy.helper import  is_isbn_or_key
 from yushu_book import YuShuBook
 
