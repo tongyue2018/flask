43e313fac1d52d1736c128db1bc18a7eb8521cca
tongyue2018 <tongyue2018@163.com>
2020-02-04 11:00:18 +0800

    nohup

--- flaskMukewang/fisher/helper.py
+++ flaskMukewang/fisher/helper.py
@@ -8,6 +8,6 @@ def is_isbn_or_key(word):
     if len(word) == 13 and word.isdigit():
         isbn_or_key = 'isbn'
     short_word = word.replace('-','') #去掉'-'判断是否是isbn10
-    if len(word) == 10 and short_word.isdigit() and len(short_word)==10:
+    if '-' in word and len(word) == 10 and short_word.isdigit():
         isbn_or_key = 'isbn'
     return isbn_or_key
