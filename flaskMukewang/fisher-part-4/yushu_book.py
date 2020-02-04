ad06271bc9e1370e57529fe3ea50e64f945ff697
tongyue2018 <tongyue2018@163.com>
2020-02-04 16:03:13 +0800

    nohup

---
+++ flaskMukewang/fisher-part-4/yushu_book.py
@@ -0,0 +1,17 @@
+
+from httpRequest import HttpRequest
+class YuShuBook:
+
+    isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
+    keyword_url = 'http://t.yushu.im/v2/book/search?q={}&start={}&count={}'
+
+    def search_by_isbn(self,isbn):
+        url = YuShuBook.isbn_url.format(isbn)
+        result = HttpRequest.getBookInfo(url)
+        return result
+
+    def search_by_key(self,keyWord,start=0,count=15):
+        url = self.keyword_url.format(keyWord,start,count)
+        result = HttpRequest.getBookInfo(url)
+        return result
+
