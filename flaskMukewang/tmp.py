bcaa5c0f91a07b3fc7ef4735597171a47e5db7c5
tongyue2018 <tongyue2018@163.com>
2020-02-07 10:10:42 +0800

    nohup

---
+++ flaskMukewang/tmp.py
@@ -0,0 +1,22 @@
+
+
+import  json
+
+dictData = {
+    "bookName":"yushu",
+    "bookType":"ISBN"
+}
+
+def writeJson():
+    with open('./data.txt','w',encoding='utf-8') as  file:
+        file.write(json.dumps(dictData,indent=2))
+        file.close()
+
+def readJson():
+    with open('./data.txt','r',encoding='utf-8') as file:
+        jsonData = file.read()
+        jsonDic = json.loads(jsonData)
+        print(jsonDic['bookName'])
+
+writeJson()
+readJson()
